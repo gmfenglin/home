5ad9111a6d3616186ff3e02c261ab75484b833c4
fenglin <gmfenglin@gmail.com>
2019-11-02 05:18:08 +0800

    layout center

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+var htmlWidth = document.documentElement.clientWidth ||document.body.clientWidth;
+var htmlDom = document.getElementsByTagName("html")[0];
+htmlDom.style.fontSize = htmlWidth/10+ "px";
 new Vue({
   el:'#app', // 
-  template:'<img src="./img/inHome.png"></img>'
+  template:'<div id="app"><img src="./img/inHome.png"></img></div>'
   });
