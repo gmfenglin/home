8166bd0ed010d52e51ce2b887fe3a83efc9d0941
fenglin <gmfenglin@gmail.com>
2019-11-02 05:28:01 +0800

    layout width fit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-var htmlWidth = document.documentElement.clientWidth ||document.body.clientWidth;
-var htmlDom = document.getElementsByTagName("html")[0];
-htmlDom.style.fontSize = htmlWidth/10+ "px";
 new Vue({
   el:'#app', // 
   template:'<div id="app"><img src="./img/inHome.png"></img></div>'
