2ef76927458eca5fe95887661dc61c3a8ce363fd
fenglin <gmfenglin@gmail.com>
2019-11-02 06:08:03 +0800

    layout width fit fixed isMobile

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
+
 new Vue({
   el:'#app', // 
-  template:'<div id="app"><img src="./img/inHome.png"></img></div>'
-  });
+  template:'<div id="app"><img src="./img/inHome.png"></img></div>',
+  mounted:function(){
+	    var isMobile=/Android|webOS|iPhone|iPod|BlackBery/i.test(navigator.userAgent);
+		if(isMobile){
+			var htmlWidth = document.documentElement.clientWidth ||document.body.clientWidth;
+			document.getElementById('app').style.width=htmlWidth;
+		}else{
+			
+		}
+  }
+});
